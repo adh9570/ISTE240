730739725f84572752617f9b3b4befa3fd244313
inasenpenguin <edthetiger@gmail.com>
2019-12-03 00:33:32 -0500

    Q&A update

--- q&a.php
+++ q&a.php
@@ -86,21 +86,114 @@ if ($conn){
                     echo"<h2>Thank you for your Question.</h2>";
                 }
             ?>
-            <h1>Questions and Answers</h1>
-            <p>Here is a place where you can see some of the most frequently asked questions about Linux and there answers.
-                Some of the things nmight be outside the scope of this site therfore we have provided some handy links to external resources.
-
+            body id="contain">
+    <div id="pagewrap">
+        <header>
+                <div class='mascot'>
+                <img class='tux' src="./assets/media/tux.png" alt="Picture of Tux">
+                <h1>Pragmatic Penguin</h1>
+                </div>
+        </header>
+        <nav class="fix">
+            <div class='back' onclick='toggle()'>
+                <div class="square">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            <div>
+            <div id='menu' class='hide'>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="#">Why Linux</a>
+                        <ul>
+                            <li><a href="files.html">File System</a></li>
+                            <li><a href="bash.html">How to Bash Shell</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="mint.html">Linux Mint</a></li>
+                    <li><a href="#">Linux Features</a>
+                        <ul>
+                            <li><a href="customization.html">Customization</a></li>
+                            <li><a href="add-ons.html">Add-Ons</a></li>
+                        </ul>
+                    </li>
+                    <li style="float: right"><a href="#">About Linux</a>
+                        <ul>
+                            <li><a href="history.html">History</a></li>
+                            <li class="active"><a href="q&a.html">Q & A</a></li>
+                            <li><a href="joinUs.php">Join Us!</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <main class="qa">
+                <div class="space"> 
+                        <br>
+                        <h2>Questions?</h2>
+                        <hr>
+                        <br>                                                
+            <p>
+                Getting to learn and use a new operating system can be difficult and frustrating at times. It's no easy task for most people! Below you will
+                find a list of linux blogs, and websites that you can visit to help further your knowledge beyond the scope of this site, and also
+                if you encounter a problem along the way chances are someone else has to, and they published how to fix it. Enjuy your linux adventure!!
             </p>
-            <ul>
-                <li><h2>How can I run EXEs of linux</h2><p>Wine is a program that can be used to run certian .exe files. Other wise linux read alternatives exist. </p></li>
-                <li><h2>Cna I run windows and linux at the same time?</h2><p>Yes you can dual boot a system to alow for wWIndows and linux to exist on the same PC.</p></li>
-            </ul>
-            <h2>Links to external sources</h2>
-            <ul>
-                <li>www.google.com</li>
-                <li>www.google.com</li>
-            </ul>
-            
+            <br><br>
+            <ol>
+                <div class="service">
+                <a href= "https://www.tecmint.com/" target="_blank">
+                    <img id="leftIMG" src="assets/media/techmint.png" height="200" width="250" alt="TecMint Logo">
+                </a>
+
+                <li id="tecMint"> <a href="https://www.tecmint.com/" target="_blank">TecMint: Linux How To and Tutorials</a>
+                
+                    <p>Tecmint is a great resource to learn about new linux distros, read articles, learn cool tips and learn more about Shell
+                    scripting and bashing.
+                    </p>
+
+                </li>
+            </div>
+                <div class="service">
+                <a href= "https://www.opendesktop.org/" target="_blank">
+                    <img id="rightIMG" src="assets/media/opendesktop.png" height="200" width="250" alt="OpenDesktop Logo">
+                </a>
+                <br><br>
+                <li> <a href="https://www.opendesktop.org/" target="_blank">OpenDesktop</a>
+                
+                    <p>OpenDesktop is a great place to go if you are looking for themes, fonts, icons, and all the newest stuff to customize 
+                        your linux expirience. 
+                    </p>
+
+                </li>
+            </div>
+                <div class="service">
+                <a href= "https://www.linux.org/forums/" target="_blank">
+                    <img id="leftIMG" src="assets/media/linuxForum.png" height="200" width="250" alt="LinuxForum Logo">
+                </a>
+
+                <li> <a href="https://www.linux.org/forums/" target="_blank">LinuxForum</a>
+                
+                    <p>If you are having a hard time completing the install, trying to update, or just a general question, post it to this
+                        forum and the linux community will be out to help you in no time.
+                    </p>
+
+                </li>
+                <br><br><br>
+            </div>
+                <div class="service">
+                <a href= "https://forums.linuxmint.com/" target="_blank">
+                    <img id="rightIMG" src="assets/media/mintLogo.jpg" height="200" width="250" alt="Linux Mint Forum Logo">
+                </a>
+                <br><br>
+                <li> <a href="https://forums.linuxmint.com/" target="_blank">Linux Mint Forum</a>
+                
+                    <p>If you are having trouble with linux mint, or just need some advicegive this forum a try. It is all about linux mint only!
+                    </p>
+
+                </li>
+            </div>
+            </ol>
             <form action="q&a.php" method="get">
                 Name:<input type="text" id ="name" name="name"><br>
                 Email:<input type="text" id ="email" name="email"><br>
