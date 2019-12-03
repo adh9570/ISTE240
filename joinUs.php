40a006561f680aed611c6d69102e580a9b60a16e
Alexis Halbur <adh9570@rit.edu>
2019-12-03 00:07:26 -0500

    refixed header

--- joinUs.php
+++ joinUs.php
@@ -10,18 +10,20 @@
 <body id="contain">
     <div id="pagewrap">
         <header>
-                <img id='tux' src="./assets/media/tux.png" alt="Picture of Tux">
+                <div class='mascot'>
+                <img class='tux' src="./assets/media/tux.png" alt="Picture of Tux">
                 <h1>Pragmatic Penguin</h1>
+                </div>
         </header>
-        <nav>
-                <div class='back' onclick='toggle()'>
-                        <div class="square">
-                            <div class="bar1"></div>
-                            <div class="bar2"></div>
-                            <div class="bar3"></div>
-                        </div>
-                    <div>
-                    <div id='menu' class='hide'>
+        <nav class="fix">
+            <div class='back' onclick='toggle()'>
+                <div class="square">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            <div>
+            <div id='menu' class='hide'>
                 <ul>
                     <li><a href="index.html">Home</a></li>
                     <li><a href="#">Why Linux</a>
@@ -34,14 +36,14 @@
                     <li><a href="#">Linux Features</a>
                         <ul>
                             <li><a href="customization.html">Customization</a></li>
-                            <li><a href="add-ons.html">Add-Ons</a></li>
+                            <li class="active"><a href="add-ons.html">Add-Ons</a></li>
                         </ul>
                     </li>
                     <li style="float: right"><a href="#">About Linux</a>
                         <ul>
                             <li><a href="history.html">History</a></li>
                             <li><a href="q&a.html">Q & A</a></li>
-                            <li class="active"><a href="joinUs.php">Join Us!</a></li>
+                            <li><a href="joinUs.php">Join Us!</a></li>
                         </ul>
                     </li>
                 </ul>
