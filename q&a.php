b592cf2e63ae28b6b95c589d128fd65720a81c31
Alexis Halbur <adh9570@rit.edu>
2019-12-03 08:31:45 -0500

    fixed images

--- q&a.php
+++ q&a.php
@@ -44,42 +44,44 @@ if ($conn){
 <body id="contain">
     <div id="pagewrap">
         <header>
-                <img id='tux' src="./assets/media/tux.png" alt="Picture of Tux">
+                <div class='mascot'>
+                <img class='tux' src="./assets/media/tux.png" alt="Picture of Tux">
                 <h1>Pragmatic Penguin</h1>
+                </div>
         </header>
-        <nav>
-                <div class='back' onclick='toggle()'>
-                        <div class="square">
-                            <div class="bar1"></div>
-                            <div class="bar2"></div>
-                            <div class="bar3"></div>
-                        </div>
-                    <div>
-                    <div id='menu' class='hide'>
+        <nav class="fix">
+            <div class='back' onclick='toggle()'>
+                <div class="square">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            <div>
+            <div id='menu' class='hide'>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="#">Why Linux</a>
                         <ul>
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="#">Why Linux</a>
-                                <ul>
-                                    <li><a href="files.html">File System</a></li>
-                                    <li><a href="bash.html">How to Bash Shell</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="mint.html">Linux Mint</a></li>
-                            <li><a href="#">Linux Features</a>
-                                <ul>
-                                    <li><a href="customization.html">Customization</a></li>
-                                    <li><a href="add-ons.html">Add-Ons</a></li>
-                                </ul>
-                            </li>
-                            <li style="float: right"><a href="#">About Linux</a>
-                                <ul>
-                                    <li><a href="history.html">History</a></li>
-                                    <li><a href="q&a.php">Q & A</a></li>
-                                    <li><a href="joinUs.php">Join Us!</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="files.html">File System</a></li>
+                            <li><a href="bash.html">How to Bash Shell</a></li>
                         </ul>
-                    </div>
+                    </li>
+                    <li><a href="mint.html">Linux Mint</a></li>
+                    <li><a href="#">Linux Features</a>
+                        <ul>
+                            <li><a href="customization.html">Customization</a></li>
+                            <li><a href="add-ons.html">Add-Ons</a></li>
+                        </ul>
+                    </li>
+                    <li style="float: right"><a href="#">About Linux</a>
+                        <ul>
+                            <li><a href="history.html">History</a></li>
+                            <li class="active"><a href="q&a.html">Q & A</a></li>
+                            <li><a href="joinUs.php">Join Us!</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </nav>
         <main>
         <?php
@@ -103,7 +105,7 @@ if ($conn){
             <ol>
                 <div class="service">
                 <a href= "https://www.tecmint.com/" target="_blank">
-                    <img id="leftIMG" src="assets/media/techmint.png" height="200" width="250" alt="TecMint Logo">
+                    <img id="leftIMG" src="assets/media/techMint.png" height="200" width="250" alt="TecMint Logo">
                 </a>
 
                 <li id="tecMint"> <a href="https://www.tecmint.com/" target="_blank">TecMint: Linux How To and Tutorials</a>
