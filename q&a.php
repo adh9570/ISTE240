a9ceabaf94fb81d69d1bcceee215934447f96374
inasenpenguin <edthetiger@gmail.com>
2019-12-03 00:51:35 -0500

    fixed?

--- q&a.php
+++ q&a.php
@@ -86,48 +86,7 @@ if ($conn){
                     echo"<h2>Thank you for your Question.</h2>";
                 }
             ?>
-            body id="contain">
-    <div id="pagewrap">
-        <header>
-                <div class='mascot'>
-                <img class='tux' src="./assets/media/tux.png" alt="Picture of Tux">
-                <h1>Pragmatic Penguin</h1>
-                </div>
-        </header>
-        <nav class="fix">
-            <div class='back' onclick='toggle()'>
-                <div class="square">
-                    <div class="bar1"></div>
-                    <div class="bar2"></div>
-                    <div class="bar3"></div>
-                </div>
-            <div>
-            <div id='menu' class='hide'>
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="#">Why Linux</a>
-                        <ul>
-                            <li><a href="files.html">File System</a></li>
-                            <li><a href="bash.html">How to Bash Shell</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="mint.html">Linux Mint</a></li>
-                    <li><a href="#">Linux Features</a>
-                        <ul>
-                            <li><a href="customization.html">Customization</a></li>
-                            <li><a href="add-ons.html">Add-Ons</a></li>
-                        </ul>
-                    </li>
-                    <li style="float: right"><a href="#">About Linux</a>
-                        <ul>
-                            <li><a href="history.html">History</a></li>
-                            <li class="active"><a href="q&a.html">Q & A</a></li>
-                            <li><a href="joinUs.php">Join Us!</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+
         <main class="qa">
                 <div class="space"> 
                         <br>
